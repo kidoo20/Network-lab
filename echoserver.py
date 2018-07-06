e039354dfa5f619154acd39db54084b5b537f915
kidoo20 <40885396+kidoo20@users.noreply.github.com>
2018-07-06 15:09:52 +0530

    Create echoserver.py

---
+++ echoserver.py
@@ -0,0 +1,12 @@
+import socket
+UDP_IP_ADDRESS="10.1.24.143"
+UDP_PORT_NO=6000
+serverSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverSock.bind((UDP_IP_ADDRESS,UDP_PORT_NO))
+print("server is waiting");
+while True :
+ data,addr=serverSock.recvfrom(1024)
+ print("Message",data)
+ print("address",addr)
+ serverSock.sendto(data,addr)
+ 
